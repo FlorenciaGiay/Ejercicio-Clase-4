db366f8d81360d6566090deb036a7a49dced987d
FlorenciaGiay <floo.giay@gmail.com>
2020-04-17 21:28:49 -0300

    Resolucion de puntos del 1 hasta el 5.

---
+++ EjercicioClase4/Logica/Extensiones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public static class Extensiones
+    {
+        public static int RetornarCantidadPalabras(this string cadena)
+        {
+            string[] arregloPalabras = cadena.Split(' ');
+            return arregloPalabras.Count();
+        }
+
+        public static int RetornarCantidadPalabras(this string cadena, char separador)
+        {
+            string[] arregloPalabras = cadena.Split(separador);
+            return arregloPalabras.Count();
+        }
+
+        public static decimal CalcularDiferenciaFechas(this DateTime fechaMayor, DateTime fechaMenor)
+        {
+            return Convert.ToDecimal((fechaMayor - fechaMenor).TotalSeconds) / 60;
+        }
+
+        public static double DevolverPromedio(this List<int> listaNumeros)
+        {
+            return Convert.ToDouble(listaNumeros.Average());
+
+        }
+
+        public static int ObtenerMultipicacionSumaParesRestaImpares(this List<int> listaNumeros)
+        {
+            return (listaNumeros.FindAll(x => x % 2 == 0).Sum()) * ((listaNumeros.First(x=>x % 2 != 0)*2) + ((listaNumeros.FindAll(x => x % 2 != 0).Sum())*-1));
+        }
+    }
+}
